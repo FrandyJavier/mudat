35d092369ad784c982384c4209ce085b6f178fdf
Frandy Javier AP <frandyjavierap@gmail.com>
2017-11-29 00:02:30 -0400

    test de categorias.

---
+++ app/src/androidTest/java/com/itla/mudat/test/testCategorias.java
@@ -0,0 +1,37 @@
+package com.itla.mudat.test;
+
+import android.test.InstrumentationTestCase;
+
+import com.itla.mudat.dao.CategoriasDbo;
+import com.itla.mudat.dao.UsuariosDbo;
+import com.itla.mudat.entity.Categoria;
+import com.itla.mudat.entity.TipoUsuario;
+import com.itla.mudat.entity.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Frandy Javier AP on 11/29/2017.
+ */
+
+public class testCategorias extends InstrumentationTestCase {
+
+    public void test_Crear() {
+
+        Categoria categoria = new Categoria();
+        CategoriasDbo db = new CategoriasDbo(getInstrumentation().getTargetContext());
+
+        categoria.setDescripcion("Prueba");
+
+        assertTrue(db.crear(categoria) > 0);
+    }
+
+    public void testListar() {
+        CategoriasDbo db = new CategoriasDbo(getInstrumentation().getTargetContext());
+        List<Categoria> categorias = new ArrayList<>();
+
+        categorias = db.listar();
+        assertTrue(categorias.size() > 0);
+    }
+}
